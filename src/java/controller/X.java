c04eaf62eda7728e013da20b7941108c7e084263
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-03 09:46:33 +0530

    Email Sending Part Ok
    📌Mail.java class in Model👇
    
    package model;
    
    import java.util.Properties;
    import javax.mail.Message;
    import javax.mail.MessagingException;
    import javax.mail.PasswordAuthentication;
    import javax.mail.Session;
    import javax.mail.Transport;
    import javax.mail.internet.InternetAddress;
    import javax.mail.internet.MimeMessage;
    
    public class Mail {
    
        private static final String APP_EMAIL = "ishara.macna@gmail.com";
        private static final String APP_PASSWORD = "hudm wcgo eobr iotm"; // Use an app-specific password here
    
        public static void sendMail(String email, String subject, String content) {
    
            Properties props = new Properties();
            props.put("mail.smtp.auth", "true");
            props.put("mail.smtp.starttls.enable", "true"); // Enable STARTTLS
            props.put("mail.smtp.host", "smtp.gmail.com");
            props.put("mail.smtp.port", "587");
            props.put("mail.smtp.ssl.protocols", "TLSv1.2"); // Force TLSv1.2
    
            // Enable debugging to view the communication with the email server
            props.put("mail.debug", "true");
    
            Session session = Session.getInstance(props,
                    new javax.mail.Authenticator() {
                @Override
                protected PasswordAuthentication getPasswordAuthentication() {
                    return new PasswordAuthentication(Mail.APP_EMAIL, Mail.APP_PASSWORD);
                }
            });
    
            try {
                Message message = new MimeMessage(session);
                message.setFrom(new InternetAddress(Mail.APP_EMAIL));
                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
                message.setSubject(subject);
                message.setText(content);
    
                Transport.send(message);
                System.out.println("Email sent successfully!");
    
            } catch (MessagingException e) {
                e.printStackTrace(); // Print the stack trace for debugging purposes
            }
        }
    }

---
+++ src/java/controller/X.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Mail;
+
+/**
+ *
+ * @author isharaLakshitha
+ */
+@WebServlet(name = "X", urlPatterns = {"/X"})
+public class X extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Mail.sendMail("lakshithahitibandara@gmail.com", "Mail Subject Here", "Body content of the Mail");
+        
+    }
+
+}
---
+++ src/java/model/Mail.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Mail {
+
+    private static final String APP_EMAIL = "ishara.macna@gmail.com";
+    private static final String APP_PASSWORD = "hudm wcgo eobr iotm"; // Use an app-specific password here
+
+    public static void sendMail(String email, String subject, String content) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true"); // Enable STARTTLS
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2"); // Force TLSv1.2
+
+        // Enable debugging to view the communication with the email server
+        props.put("mail.debug", "true");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(Mail.APP_EMAIL, Mail.APP_PASSWORD);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(Mail.APP_EMAIL));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+            System.out.println("Email sent successfully!");
+
+        } catch (MessagingException e) {
+            e.printStackTrace(); // Print the stack trace for debugging purposes
+        }
+    }
+}
